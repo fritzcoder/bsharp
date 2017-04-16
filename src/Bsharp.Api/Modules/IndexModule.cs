139e306bb7dab3a2be50b54588e6e0b711d6c697
fritzcoder <fredrick.seitz@gmail.com>
2017-04-16 14:52:29 -0400

    Initial commit
    Adds basic scaffolding for .net core nancyfx project

---
+++ src/Bsharp.Api/Modules/IndexModule.cs
@@ -0,0 +1,12 @@
+namespace Bsharp.Api
+{
+    using Nancy;
+
+    public class IndexModule : NancyModule
+    {
+        public IndexModule()
+        {
+            Get("/", _ => "Tune it to Bsharp");
+        }
+    }
+}
